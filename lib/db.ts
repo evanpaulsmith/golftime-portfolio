9fe0f1eb95eef1d7c80ede432192e49c3504c9f8
Evan Paul Smith <your-email@example.com>
2026-09-20 20:57:57 -0400

    Configure PostgreSQL for Neon deployment

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: Number(process.env.PGPORT ?? 5432),
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
